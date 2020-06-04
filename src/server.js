19eb88ae5edcd8613669e766918ee42572e82afd
unknown <vitorjao56@gmail.com>
2020-06-04 15:18:59 -0300

    refatorados todos os html para o uso do nunjucks

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express= require('express');
+const server = express();
+
+//styles config
+
+server.use(express.static("public"));
+
+const nunjucks = require('nunjucks')
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
+
+
+
+server.get("/", (req,res)=>{
+    res.render("index.html");
+})
+
+
+server.get("/create-point", (req,res)=>{
+    res.render("create-point.html");
+})
+
+
+server.get("/search", (req,res)=>{
+    res.render("search-results.html");
+})
+
+
+
+
+server.listen(3000)
